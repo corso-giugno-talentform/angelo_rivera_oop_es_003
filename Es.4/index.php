29c686913ae9ec790f84cd7434f5e419ca28eb27
Arwel219 <aruelangeloriv01@gmail.com>
2025-07-22 17:49:51 +0200

    aggiornamento esercizio 4

--- Es.4/index.php
+++ Es.4/index.php
@@ -21,7 +21,7 @@ class Computer
 
     public function infoGet()
     {
-        echo " " . $this->computer . "\n" . " RAM: " . $this->ram . "\n Scheda Video: " . $this->schedavideo . "\n Processore: " . $this->processore;
+        echo " " . $this->computer . "\n\n" . " RAM: " . $this->ram . "\n Scheda Video: " . $this->schedavideo . "\n Processore: " . $this->processore;
     }
 }
 
